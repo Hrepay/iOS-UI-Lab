2e3cbd3150a105c3b0f4a4219f6152eeca59b431
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-09-03 14:46:32 +0900

    UILab3: 네비게이션바 커스텀

---
+++ UI_Lab/Sources/Labs/Lab3 /mainView.swift
@@ -0,0 +1,31 @@
+//
+//  mainView.swift
+//  UI_Lab
+//
+//  Created by 황상환 on 8/4/25.
+//
+
+import SwiftUI
+
+struct mainView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Hello, World!")
+
+                NavigationLink(destination: NavigationView()) {
+                    Text("다음 화면으로 이동")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            .navigationTitle("Main View")
+        }
+    }
+}
+
+#Preview {
+    mainView()
+}
